98679fcdd70155adbf2dcfa35212d90f988f9bc3
ludafa <ludafa@outlook.com>
2023-11-10 16:55:21 +0800

    use my own dotfiles repo

--- install.sh
+++ install.sh
@@ -2,25 +2,16 @@
 
 zshrc() {
   echo "==========================================================="
-  echo "             cloning dotfiles                              "
-  echo "-----------------------------------------------------------"
-  git clone https://github.com/joshjohanning/dotfiles ~/dotfiles --depth=1
-
-  echo "==========================================================="
-  echo "             installing amix/vimrc                              "
+  echo "             installing amix/vimrc                         "
   echo "-----------------------------------------------------------"
   git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_awesome_vimrc.sh
 
   echo "==========================================================="
-  echo "             cloning zsh-autosuggestions                   "
+  echo "             install zsh plugins                           "
   echo "-----------------------------------------------------------"
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-  echo "==========================================================="
-  echo "             cloning zsh-syntax-highlighting               "
-  echo "-----------------------------------------------------------"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
   echo "==========================================================="
   echo "             cloning powerlevel10k                         "
@@ -30,22 +21,22 @@ zshrc() {
   echo "==========================================================="
   echo "             import zshrc                                  "
   echo "-----------------------------------------------------------"
-  cat ~/dotfiles/zshrc > ~/.zshrc
+  cat ./zshrc > ~/.zshrc
 
   echo "==========================================================="
   echo "             import powerlevel10k                          "
   echo "-----------------------------------------------------------"
-  cat ~/dotfiles/p10k.zsh > ~/.p10k.zsh
+  cat ./p10k.zsh > ~/.p10k.zsh
 
   echo "==========================================================="
   echo "             import vimrc                                  "
   echo "-----------------------------------------------------------"
-  cat ~/dotfiles/vimrc.vim > ~/.vim_runtime/my_configs.vim
+  cat ./vimrc.vim > ~/.vim_runtime/my_configs.vim
 
   echo "==========================================================="
   echo "             import git config                             "
   echo "-----------------------------------------------------------"
-  cat ~/dotfiles/gitconfig > ~/.gitconfig
+  cat ./gitconfig > ~/.gitconfig
 
   echo "==========================================================="
   echo "             installing fnm                                "
